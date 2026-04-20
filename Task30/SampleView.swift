9292c44d23b912d98428529e8b852a1793e44fff
BT_C09_01 <mobileappdev@bths.edu>
2026-04-20 14:36:36 -0400

    added SampleView file

---
+++ Task30/SampleView.swift
@@ -0,0 +1,18 @@
+//
+//  SampleView.swift
+//  Task30
+//
+//  Created by Student on 4/20/26.
+//
+
+import SwiftUI
+
+struct SampleView: View {
+    var body: some View {
+        Text("look at this sample view")
+    }
+}
+
+#Preview {
+    SampleView()
+}
