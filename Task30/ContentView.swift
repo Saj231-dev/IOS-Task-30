5db6e8ab2f485c73c136a1f5cce7c4d7a76a6ec4
BT_C09_01 <mobileappdev@bths.edu>
2026-04-20 14:38:34 -0400

    changed contentview and sampleview texts and images

--- Task30/ContentView.swift
+++ Task30/ContentView.swift
@@ -10,10 +10,11 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "flag")
+            Image(systemName: "heart")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Git")
+            Text("I LOVE GIT")
         }
         .padding()
     }
--- Task30/SampleView.swift
+++ Task30/SampleView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct SampleView: View {
     var body: some View {
-        Text("look at this sample view")
+        
     }
 }
 
